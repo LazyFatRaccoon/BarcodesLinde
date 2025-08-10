081ecf4e4cdba55107d1302720c1fb0d4f22eb6d
Vladimir Todorov <vladimir.todorov.ukraine@gmail.com>
2025-08-10 13:39:58 +0300

    url fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,6 @@
-export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4000/api";
+const PROD_API =
+  typeof window !== "undefined" ? `${window.location.origin}/api` : "/api";
+
+export const API_URL =
+  import.meta.env.VITE_API_URL || // якщо явно задано
+  (import.meta.env.PROD ? PROD_API : "http://localhost:4000/api");
